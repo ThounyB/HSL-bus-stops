423fb5f4313aeac037b89dd2c72aaae9827091dc
Thouny89 <thouny.web@gmail.com>
2024-02-24 13:14:37 +0200

    WIP

--- src/components/map/OLMap.tsx
+++ src/components/map/OLMap.tsx
@@ -5,6 +5,44 @@ import View from "ol/View";
 import TileLayer from "ol/layer/Tile";
 import OSM from "ol/source/OSM";
 import "./olMap.css";
+import GeoJSON from "ol/format/GeoJSON";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import Feature from "ol/Feature";
+import Geometry from "ol/geom/Geometry";
+import MapBrowserEvent from "ol/MapBrowserEvent";
+
+const geojsonData = {
+    type: "FeatureCollection",
+    features: [
+        {
+            type: "Feature",
+            geometry: {
+                type: "Point",
+                coordinates: [3286012.0935983104, 9006746.720218109],
+            },
+            properties: {
+                name: "Koti",
+            },
+        },
+    ],
+};
+
+// Create a new GeoJSON format parser instance
+const geoJSONFormat = new GeoJSON();
+
+// Parse the GeoJSON data into an OpenLayers feature
+const features = geoJSONFormat.readFeatures(geojsonData);
+
+// Create a vector source and add the features
+const vectorSource = new VectorSource({
+    features: features, // Pass an array of features
+});
+
+// Create a vector layer and add the source
+const vectorLayer = new VectorLayer({
+    source: vectorSource as VectorSource<Feature<Geometry>>,
+});
 
 function OLMap() {
     const mapRef = useRef(null);
@@ -18,12 +56,25 @@ function OLMap() {
                 new TileLayer({
                     source: new OSM(),
                 }),
+                vectorLayer,
             ],
             view: new View({
-                center: [0, 0],
-                zoom: 1,
+                center: [3282802.75875484, 8989233.876836976], // Start from Joensuu
+                zoom: 9.377050107672698,
             }),
         });
+
+        function coords(e: MapBrowserEvent<PointerEvent>) {
+            const view = map.getView();
+            const center = view.getCenter();
+            const zoom = view.getZoom();
+            console.log("View Center:", center);
+            console.log("Zoom Level:", zoom);
+
+            console.log("click coords ", e.coordinate);
+        }
+
+        map.on("click", (e) => coords(e));
 
         return () => {
             map.dispose();
